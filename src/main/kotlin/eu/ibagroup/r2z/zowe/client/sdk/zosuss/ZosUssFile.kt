ac1e86f499d645b4f4b353cf352bc34d1ed188b6
Valiantsin Studzenichnik <wishmaster121@mail.ru>
2022-09-23 13:01:27 +0300

    IJMP-702 USS file creation and test

---
+++ src/main/kotlin/eu/ibagroup/r2z/zowe/client/sdk/zosuss/ZosUssFile.kt
@@ -0,0 +1,43 @@
+// Copyright © 2020 IBA Group, a.s. All rights reserved. Use of this source code is governed by Eclipse Public License – v 2.0 that can be found at: https://www.eclipse.org/legal/epl-2.0/
+
+package eu.ibagroup.r2z.zowe.client.sdk.zosuss
+
+import eu.ibagroup.r2z.*
+import eu.ibagroup.r2z.zowe.client.sdk.core.ZOSConnection
+import okhttp3.Credentials
+import okhttp3.OkHttpClient
+import retrofit2.Response
+
+class ZosUssFile (
+    var connection: ZOSConnection,
+    var httpClient: OkHttpClient = UnsafeOkHttpClient.unsafeOkHttpClient
+) {
+    init {
+        connection.checkConnection()
+    }
+
+    var response: Response<*>? = null
+
+    /**
+     * Creates a new file or directory with specified parameters
+     *
+     * @param filePath path of the file or directory (e.g. u/jiahj/text.txt)
+     * @param params create USS file parameters, see CreateUssFile class
+     * @return http response object
+     * @throws Exception error processing request
+     */
+    fun createFile(filePath: String, params: CreateUssFile): Response<*> {
+        val url = "${connection.protocol}://${connection.host}:${connection.zosmfPort}"
+        val dataApi = buildApi<DataAPI>(url, httpClient)
+        val call = dataApi.createUssFile(
+            authorizationToken = Credentials.basic(connection.user, connection.password),
+            filePath = FilePath(filePath),
+            body = params
+        )
+        response = call.execute()
+        if (response?.isSuccessful != true) {
+            throw Exception(response?.errorBody()?.string())
+        }
+        return response ?: throw Exception("No response returned")
+    }
+}
